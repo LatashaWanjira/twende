a6deb665652b8fe9f7470e29d1c2b4d4fff9a6a8
Kelvin Bunei <keighb@live.com>
2016-12-15 05:36:17 +0300

    updated js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,65 @@
+var named = new Array();
+var numbered = new Array();
+var locations = new Array();
+var time = new Array();
+var destination = new Array();
+var space = new Array();
+
+function insert() {
+    var namedValue = document.getElementById('named').value;
+    named[named.length] = namedValue;
+    var numberedValue = document.getElementById('numbered').value;
+    numbered[numbered.length] = numberedValue;
+    var locationsValue = document.getElementById('location').value;
+    locations[locations.length] = locationsValue;
+    var timeValue = document.getElementById('time').value;
+    time[time.length] = timeValue;
+    var destinationValue = document.getElementById('destination').value;
+    destination[destination.length] = destinationValue;
+    var spaceValue = document.getElementById('space').value;
+    space[space.length] = spaceValue;
+
+}
+
+function show() {
+    var content = "<b>Drivers Details:</b><br>";
+    for (var i = 0; i < named.length; i++) {
+        content += named[i] + "<br>";
+    };
+    for (var i = 0; i < numbered.length; i++) {
+        content += numbered[i] + "<br>";
+    };
+
+    for (var i = 0; i < locations.length; i++) {
+        content += locations[i] + "<br>";
+    };
+    for (var i = 0; i < time.length; i++) {
+        content += time[i] + "<br>";
+    };
+    for (var i = 0; i < destination.length; i++) {
+        content += destination[i] + "<br>";
+    };
+    for (var i = 0; i < space.length; i++) {
+        content += space[i] + "<br>";
+    };
+    document.getElementById('display').innerHTML = content;
+};
+
+
+$(document).ready(function() {
+        $("#show").click
+
+        function(show) {
+
+        });
+};
+
+
+
+
+
+
+
 var driver = {
     name: "moses",
     route: "town service",
@@ -7,47 +69,60 @@ var driver = {
 };
 var driver1 = {
     name: "adelle",
-    route: "Ngong",
+    route: "burubur",
     time: "morning",
     rate: 90,
-    contact: +254711100111
-};
-var driver2 = {
-    name: "andrew",
-    route: "Karen",
-    time: "morning",
-    rate: 100,
-    contact: +254700000222
-};
-var driver3 = {
-    name: "cornellius",
-    route: "cbd",
-    time: "evening",
-    rate: 100,
-    contact: +254712100333
+    contact: +254711104586
 };
 
+
+var driver2 = {
+    name: "morio",
+    route: "south c",
+    time: "morning",
+    rate: 90,
+    contact: +254711145111
+};
+
+
+
 $(document).ready(function() {
-    $("#karen").click(function() {
+
+    $("#ngong").click(function() {
+        $("#ddetails").show();
+    });
+});
+
+$(document).ready(function() {
+
+    $("#south").click(function() {
+        $("#ddetails").show();
+    });
+});
+
+$(document).ready(function() {
+
+    $("#buru").click(function() {
+        $("#ddetails").show();
+    });
+});
+
+$(document).ready(function() {
+    $("#ngong").click(function() {
         document.write("<li>Name: " + driver1.name + "</li>" + "<li>Route: " + driver1.route + "</li>" + "<li>Time: " + driver1.time + "</li>" + "<li>Contact: " + driver1.contact + "</li>");
     });
 });
 
 $(document).ready(function() {
-    $("#cbd").click(function() {
+    $("#buru").click(function() {
+        document.write("<li>Name: " + driver.name + "</li>" + "<li>Route: " + driver.route + "</li>" + "<li>Time: " + driver.time + "</li>" + "<li>Contact: " + driver.contact + "</li>");
+    });
+});
+
+
+$(document).ready(function() {
+    $("#south").click(function() {
         document.write("<li>Name: " + driver2.name + "</li>" + "<li>Route: " + driver2.route + "</li>" + "<li>Time: " + driver2.time + "</li>" + "<li>Contact: " + driver2.contact + "</li>");
     });
 });
 
-
-$(document).ready(function() {
-    $("#utawala").click(function() {
-        document.write("<li>Name: " + driver3.name + "</li>" + "<li>Route: " + driver3.route + "</li>" + "<li>Time: " + driver3.time + "</li>" + "<li>Contact: " + driver3.contact + "</li>");
-    });
-});
-
-$(document).ready(function() {
-    $("#runda").click(function() {
-        document.write("<li>Name: " + driver.name + "</li>" + "<li>Route: " + driver.route + "</li>" + "<li>Time: " + driver.time + "</li>" + "<li>Contact: " + driver.contact + "</li>");
-    });
-});
